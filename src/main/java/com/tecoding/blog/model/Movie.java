e7770ef5c159e68151fcc927656dbdfe7acb775b
questzz <questzz@naver.com>
2022-06-17 16:30:55 +0900

    복합키 만드는 연습

---
+++ src/main/java/com/tecoding/blog/model/Movie.java
@@ -0,0 +1,21 @@
+package com.tecoding.blog.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Movie {
+	
+	@Id
+	@Column(length = 30)
+	private String title; 
+	
+	@Column(columnDefinition = "TEXT")
+	private String image; 
+	
+	@Column(columnDefinition = "DATE")
+	private String releaseDate;
+	
+	private float starScore; 
+}
---
+++ src/main/java/com/tecoding/blog/model/RoleId.java
@@ -0,0 +1,19 @@
+package com.tecoding.blog.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor
+public class RoleId implements Serializable {
+	
+	@Column(length = 255)
+	private String title; 
+	
+	@Column(length = 20)
+	private String actorName;
+}
